820fcfc60d501832b4b590a8c0ea4d16348660dc
Bellamkonda Sri Krishna Chaitanya <87589090+KrishnaBellamkonda@users.noreply.github.com>
2022-03-23 09:25:25 +0000

    Add files via upload
    Initial commit

---
+++ Simple Game/code3.js
@@ -0,0 +1,20 @@
+gdjs.Win_32Code = {};
+
+gdjs.Win_32Code.conditionTrue_0 = {val:false};
+gdjs.Win_32Code.condition0IsTrue_0 = {val:false};
+
+
+gdjs.Win_32Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Win_32Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Win_32Code.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['Win_32Code'] = gdjs.Win_32Code;
